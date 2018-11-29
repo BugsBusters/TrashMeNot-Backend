dd62bb9801d1b670392e825d63cc2436e1a310fd
Jacopo Iezzi <emailprova.94@libero.it>
2018-11-29 16:00:15 +0100

    Ordini testata FINITO

---
+++ tests/Unit/OrdiniTestataControllerTest.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peanutonetwo
+ * Date: 29/11/18
+ * Time: 15.37
+ */
+
+namespace Tests\Unit;
+
+
+use App\Model\Ordine_Testata;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class OrdiniTestataControllerTest extends TestCase
+{
+    use WithoutMiddleware;
+
+
+    public function testPut()
+    {
+        $this->json('PUT','/api/ordinetestata/',[
+            "cliente_id" => "1",
+            "venditore_id" => "1",
+            "sconto" =>"20",
+            "totale" => "200",
+        ])->assertStatus(200);
+    }
+
+    public function testAll()
+    {
+        $this->json('GET','/api/ordinitestate')->assertStatus(200);
+    }
+
+    public function testFind()
+    {
+        $id = Ordine_Testata::all()->last()->id;
+        $this->json('GET', "/api/ordinetestata/$id")->assertStatus(200);
+    }
+
+
+    public function testPatch()
+    {
+        $id = Ordine_Testata::all()->last()->id;
+        $this->json('PATCH',"/api/ordinetestata/$id",[
+            "cliente_id" => "1",
+            "venditore_id" => "1",
+            "sconto" =>"20",
+            "totale" => "200",
+        ])->assertStatus(200);
+    }
+
+
+    public function testDelete()
+    {
+        $id = Ordine_Testata::all()->last()->id;
+        $this->json('DELETE',"/api/ordinetestata/$id")->assertStatus(200);
+    }
+
+
+
+
+}
