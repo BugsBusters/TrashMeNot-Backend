6d6d10105e3a2e67a7da630a7e698f16d9391d80
Jacopo Iezzi <emailprova.94@libero.it>
2018-11-30 07:49:07 +0100

    Fix prodotti

--- database/seeds/ProdottiSeeder.php
+++ database/seeds/ProdottiSeeder.php
@@ -16,7 +16,7 @@ class ProdottiSeeder extends Seeder
         $faker = Faker::create();
         $categorie = \App\Model\Categoria::all()->pluck('id')->toArray();
         $seller_ids = \App\User::where('ruolo','venditore')->pluck('id')->toArray();
-        $prodotti = ["Tazza","Jeans","Orologio","Fertilizzante","Maschera Viso","Camicia","Sneakers","Semi Di Pomodoro","Viaggio in Albania","Monitor Full HD"];
+        $prodotti = ["Cover per telefono","Scarpe con Plastica Riciclata","Ciotole Di Vinile","Camioncino","Occhiali Da Sole","Shopping Bag","Spazzolino","Lampadina","Quaderno","Vaso","Bottiglia","Tappi di sughero","Tazza","Zaino","Papillon","T-Shirt","Libro","Bracciale","Portachiavi","Collana"];
         foreach ($prodotti as $prodotto) {
             DB::table('prodotti')->insert([
 
